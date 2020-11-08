2377f2190dac76dcc48265ec977ff917f298f304
Mitchell <mitchell@raygun.com>
2020-11-09 11:29:19 +1300

    Updated sample console app
    Updated sample console app

--- src/NLog.Raygun.TestApp/Program.cs
+++ src/NLog.Raygun.TestApp/Program.cs
@@ -6,17 +6,20 @@ namespace NLog.Raygun.TestApp
   internal class Program
   {
     private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-    
+
     private static void Main(string[] args)
     {
-      Console.WriteLine("Sending Message to RayGun...");
+      AppDomain.CurrentDomain.UnhandledException += CatchUnhandledException;
 
-      Logger.Info("This is a test!");
+      // By default all log events are sent as Raygun error reports.
+      // However log events without an exception can be recorded as a breadcrumb for the next
+      // error report if `IncludeBreadcrumbMessages` is set to `true` in your Raygun target configuration.
+      Logger.Info("Hello world!");
 
       try
       {
-        var e = new Exception("Test Exception");
-        e.Data["Tags"] = new List<string> { "Tester123" };
+        var e = new Exception("A generic handled exception");
+        e.Data["Tags"] = new List<string> { "NonFatal" };
 
         throw e;
       }
@@ -25,8 +28,30 @@ namespace NLog.Raygun.TestApp
         Logger.Error(exception);
       }
 
-      Console.WriteLine("Finished...");
-      Console.Read();
+      PerformOperation();
+    }
+
+    private static void PerformOperation()
+    {
+      PerformUnsafeOperation();
+    }
+
+    private static void PerformUnsafeOperation()
+    {
+      var zero = 0;
+      var one = 1;
+
+      var result = one / zero;
+    }
+
+    private static void CatchUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+      var exception = e.ExceptionObject as Exception;
+      exception.Data["Tags"] = new List<string> { "Fatal", "UnhandledException" };
+      Logger.Fatal(exception);
+
+      Console.WriteLine("A fatal error has occurred.\nPress any <Enter> to exit...");
+      while (Console.ReadKey().Key != ConsoleKey.Enter) {}
     }
   }
 }
